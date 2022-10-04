d20818ac2ce14f8177db366cfdda3fd27c2bf1a4
tyler <ttang7910@bths.edu>
2022-10-03 20:24:55 -0400

    changed some code in Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,22 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(80, 100);
-        Rectangle rect2 = new Rectangle(50);
-        Rectangle rect3 = new Rectangle();
+        ShoppingListItem item = new ShoppingListItem("apple", 0.50, 10);
+        System.out.println(item);
 
-        rect3 = rect1;
-        rect1 = rect2;
-        rect1.setLength(60);
-
-        Rectangle rect4 = rect3;
-        rect3.setWidth(70);
-        rect3 = rect1;
-        rect3.setLength(40);
-        rect2.setWidth(90);
-
-        System.out.println(rect1.calculateArea());
-        System.out.println(rect2.calculateArea());
-        System.out.println(rect3.calculateArea());
-        System.out.println(rect4.calculateArea());
     }
 }
